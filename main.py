a81cbbcbabd0355142163679089a4e092fade7e8
jackdante666 <73043449+jackdante666@users.noreply.github.com>
2021-01-31 14:51:04 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pygame, sys
+import math, random, sys
+import pygame
+from pygame.locals import *
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -8,35 +10,65 @@ class Player(pygame.sprite.Sprite):
 
 pygame.init()
 clock = pygame.time.Clock()
-screen_width = 1200
+W, H = 1280, 720
+HW, HH = W / 2, H / 2
+DS = pygame.display.set_mode((W, H))
+screen_width = 1000
 screen_height = 800
 screen = pygame.display.set_mode((screen_width, screen_height))
-bg = pygame.image.load('photo800x800.jpeg')
+
+AREA = W * H
 player = Player()
 player_group = pygame.sprite.Group()
 player_group.add(player)
+bg = pygame.image.load('photo800x800.jpeg').convert()
+bgWidth, bgHeight = bg.get_rect().size
+
+stageWidth = bgWidth * 2
+stagePosX = 0
+
+startScrollingPosX = HW
+
+circleRadius = 25
+circlePosX = circleRadius
+
+playerPosX = circleRadius
+playerPosY = 585
+playerVelocityX = 0
 
 while True:
 
+    screen.blit(bg, (0, 0))
+    keys = pygame.key.get_pressed()
+
+    if keys[K_RIGHT]:
+        playerVelocityX = 1
+    elif keys[K_LEFT]:
+        playerVelocityX = -1
+    else:
+        playerVelocityX = 0
+
+    playerPosX += playerVelocityX
+    if playerPosX > stageWidth - circleRadius: playerPosX = stageWidth - circleRadius
+    if playerPosX < circleRadius: playerPosX = circleRadius
+    if playerPosX < startScrollingPosX: circlePosX = playerPosX
+    elif playerPosX > stageWidth - startScrollingPosX: circlePosX = playerPosX - stageWidth + W
+    else:
+            circlePosX = startScrollingPosX
+            stagePosX += -playerVelocityX
+
+    rel_x = stagePosX % bgWidth
+    DS.blit(bg, (rel_x - bgWidth, 0))
+    if rel_x < W:
+        DS.blit(bg, (rel_x, 0))
+
+    player_group.draw(screen)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-    keys = pygame.key.get_pressed()
-
-    if keys[pygame.K_RIGHT]:
-        player.rect.x += 5
-    if keys[pygame.K_LEFT]:
-        player.rect.x -= 5
-    if keys[pygame.K_UP]:
-        player.rect.y -= 5
-    if keys[pygame.K_DOWN]:
-        player.rect.y += 5
-
-    pygame.display.update()
-    screen.blit(bg, (0, 0))
     player_group.update()
-    player_group.draw(bg)
     pygame.display.flip()
-    clock.tick(30)
+    pygame.display.update()
+    clock.tick(100)
